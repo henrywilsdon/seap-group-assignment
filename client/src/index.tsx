a2969fb9b18f7b74219d015be06127bc520a22df
Caleb Rust <redrusty12@gmail.com>
2022-08-22 18:51:25 +0930

    Added react-router to client Added client routes for home, login, athletes and courses. Issue #37

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -3,13 +3,21 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './app/App';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(
     document.getElementById('root') as HTMLElement,
 );
 root.render(
     <React.StrictMode>
-        <App />
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<App />} />
+                <Route path="/login" element={<>Login</>} />
+                <Route path="/athletes" element={<>Athletes</>} />
+                <Route path="/courses" element={<>Courses</>} />
+            </Routes>
+        </BrowserRouter>
     </React.StrictMode>,
 );
 
