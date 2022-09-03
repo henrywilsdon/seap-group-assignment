471f1c09b92247b4c87d59a90a690530c7e63340
Jack Hislop <jack.hislop@student.adelaide.edu.au>
2022-09-03 13:36:23 +0930

    Created tests cases for login, logout, register...

--- service_layer/backend/server_functions/views.py
+++ service_layer/backend/server_functions/views.py
@@ -5,7 +5,8 @@ from django.http import HttpResponse, JsonResponse
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
-from django.views.decorators.http import require_POST
+from django.views.decorators.http import require_POST, require_GET
+from django.views.decorators.http import require_http_methods
 from django.views.decorators.csrf import ensure_csrf_cookie
 from django.middleware.csrf import get_token
 
@@ -23,46 +24,72 @@ def home(request):
 @require_POST
 def register_view(request):
     user_data  = json.loads(request.body)
-    username = user_data["username"]
+    username = user_data["email"]
     email = user_data["email"]
     password = user_data["password"]
 
-    if username is None or email is None:
+    if not username_exists(username):
         user = User.objects.create_user(username, email, password)
         if "first_name" in user_data:
             first_name = user_data["first_name"]
             user.first_name = first_name
-            return JsonResponse({'detail': 'Successfully registered.'})
+        return JsonResponse({'detail': 'Successfully registered.'}, status=200)
     else:
         return JsonResponse({'detail': 'Username or email already in use'}, status=400)
 
 @require_POST
 def login_view(request):
     data = json.loads(request.body)
-    username = data["username"]
+    username = data["email"]
     password = data["password"]
 
     if username is None or password is None:
-        return JsonResponse({'detail': 'Please provide username and password.'}, status=400)
+        return JsonResponse({'detail': 'Please provide email and password.'}, status=400)
 
-    #return HttpResponse('sessionId')
     user = authenticate(request, username=username, password=password)
-    if user is not None:
+    if username_exists(username):
         login(request, user)
-        return JsonResponse({'detail': 'Successfully logged in.'})
+        return JsonResponse({'detail': 'Successfully logged in.'}, status=200)
     else:
         return JsonResponse({'detail': 'Invalid credentials.'}, status=400)
 
-
+@require_POST
 def logout_view(request):
-    if not request.user.is_authenticated:
+    data = json.loads(request.body)
+    username = data["email"]
+    password = data["password"]
+    user = authenticate(request, username=username, password=password)
+    if not user.is_authenticated:
         return JsonResponse({'detail': 'You\'re not logged in.'}, status=400)
     logout(request)
-    return JsonResponse({'detail': 'Successfully logged out.'})
+    return JsonResponse({'detail': 'Successfully logged out.'}, status=200)
 
-def user_detail(request, user_id):
-    return HttpResponse(f'<p>user_detail view with id {user_id}</p>')
-    #JSON
+@require_http_methods(["PUT"])
+def update_user_view(request):
+    data = json.loads(request.body)
+    username = data["email"]
+    currentPassword = data["currentPassword"]
+    newPassword = data["newPassword"]
+
+    user = authenticate(request, username=username, password=currentPassword)
+    if user.is_authenticated:
+        user.password = newPassword
+        return JsonResponse({'detail': 'Successfully changed password'}, status=200)
+    else: 
+        return JsonResponse({'detail': 'User not authenticated'}, status=400)
+
+""" @require_http_methods(["GET"])
+def get_user_view(request):
+    data = json.loads(request.body)
+    username = data["email"]
+    #password = data["password"]
+    user = User.objects.get(username=username)
+
+    if user.is_authenticated:
+        id = request.user.id
+        return JsonResponse({'id': '2'}, status=200)
+    else: 
+        return JsonResponse({'detail': 'Unauthorized'}, status=401) """
 
 """ def registerPage(request):
     form = UserCreationForm()
@@ -72,4 +99,7 @@ def user_detail(request, user_id):
 def csrf(request):
     return HttpResponse('check out this cookie') """
 
-#function for ensuring token
+def username_exists(username):
+    if User.objects.filter(username=username).exists():
+        return True
+    return False
