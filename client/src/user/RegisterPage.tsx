c9f617a5f1f26dc9d0e5d4ee8c31eaa9a1ef5166
a1648287 <joshua.connolly@student.adelaide.edu.au>
2022-09-03 16:01:02 +0930

    updated Layout of reg page fields

--- client/src/user/RegisterPage.tsx
+++ client/src/user/RegisterPage.tsx
@@ -10,36 +10,48 @@ export default function RegisterPage({}: Props) {
     const [username, setUsername] = useState('');
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
+    const [errorMessage, setErrorMessage] = useState('');
 
     //username
     const handleName = (event: any) => {
         setUsername(event.target.value);
+        setErrorMessage('');
     };
 
     //email
     const handleEmail = (event: any) => {
         setEmail(event.target.value);
+        setErrorMessage('');
     };
 
     //password
 
     const handlePassword = (event: any) => {
         setPassword(event.target.value);
+        setErrorMessage('');
     };
     //button click
     const handleClick = (event: any) => {
-        register(username, email, password);
+        register(username, email, password).catch((error) => {
+            setErrorMessage('error.message');
+        });
     };
 
     return (
         <div className="RegisterPage">
-            <Typography variant="h4">Register Account</Typography>
+            <Typography variant="h4" sx={{ marginBottom: 2 }}>
+                Register Account
+            </Typography>
             <TextField
                 color="primary"
                 variant="standard"
                 label="Email Address"
                 value={email}
+                required
                 onChange={handleEmail}
+                error={!!errorMessage}
+                helperText={errorMessage}
+                sx={{ marginBottom: 2 }}
             />
 
             <TextField
@@ -47,7 +59,11 @@ export default function RegisterPage({}: Props) {
                 variant="standard"
                 label="Username"
                 value={username}
+                required
                 onChange={handleName}
+                error={!!errorMessage}
+                helperText={errorMessage}
+                sx={{ marginBottom: 2 }}
             />
 
             <TextField
@@ -55,8 +71,12 @@ export default function RegisterPage({}: Props) {
                 variant="standard"
                 label="Password"
                 value={password}
+                type="password"
+                required
                 onChange={handlePassword}
-                sx={{ marginBottom: 2 }}
+                error={!!errorMessage}
+                helperText={errorMessage}
+                sx={{ marginBottom: 4 }}
             />
 
             <Button variant="contained" onClick={handleClick}>
