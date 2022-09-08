ead2289f1d0465ebbd53034432048c57641594f4
Junwen <a1702535@student.adelaide.edu.au>
2022-09-08 14:19:21 +0930

    remove unused import

--- client/src/courses/ManageCoursesPage.tsx
+++ client/src/courses/ManageCoursesPage.tsx
@@ -8,7 +8,6 @@ import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
 import { Button } from '@mui/material';
-import AthleteFormDialog from './AthleteFormDialog';
 
 /**
 Course
