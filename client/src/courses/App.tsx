1de8701d112989e642fad345e17b7cdb0a59902d
Junwen <a1702535@student.adelaide.edu.au>
2022-09-08 10:39:55 +0930

    ManageCourse

---
+++ client/src/courses/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+
+// import ManageAthletesPage from './ManageAthletesPage';
+import ManageCoursesPage from './ManageCoursesPage';
+
+// Demo of using ManageCoursesPage
+function App() {
+    // return <ManageAthletesPage />;
+    return <ManageCoursesPage />;
+}
+
+export default App;
