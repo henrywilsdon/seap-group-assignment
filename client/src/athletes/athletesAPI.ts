d50f8c9c36dbf18ec02d58d4300f4ab5f47cdc25
Caleb Rust <a1782615@adelaide.edu.au>
2022-09-09 19:39:41 +0930

    Convert backend athlete models to frontend model Create athlete working View all athletes working

--- client/src/athletes/athletesAPI.ts
+++ client/src/athletes/athletesAPI.ts
@@ -18,13 +18,28 @@ export type AthleteData = {
     wPrime: number;
 };
 
+/**
+ * Server Athlete model
+ */
+type BackendAthlete = {
+    id?: number;
+    name: string;
+    rider_mass: number;
+    bike_mass: number;
+    other_mass: number;
+    total_mass: number;
+    CP_FTP: number;
+    W_prime: number;
+};
+
 export function getAthletes(): Promise<AthleteData[]> {
     return fetch('http://localhost:8000/server_functions/athlete/', {
         method: 'GET',
         credentials: 'include',
     }).then(async (response) => {
         if (response.ok) {
-            return (await response.json()).athletes;
+            const athletes: BackendAthlete[] = (await response.json()).athletes;
+            return athletes.map<AthleteData>(backendAthleteToFrontend);
         } else {
             if (response.headers.get('Content-Type') === 'application/json') {
                 const data = await response.json();
@@ -40,22 +55,16 @@ export function updateAthlete(
     athleteId: number,
     athleteData: Omit<AthleteData, 'id'>,
 ): Promise<any> {
+    const backendAthlete = frontendAthleteToBackend(athleteData);
     return fetch(
-        'http://localhost:8000/server_functions/athlete/' + athleteId,
+        'http://localhost:8000/server_functions/athlete/' + athleteId + '/',
         {
             method: 'PUT',
             credentials: 'include',
             headers: {
                 'Content-Type': 'application/json',
             },
-            body: JSON.stringify({
-                fullName: athleteData.fullName,
-                riderMass: athleteData.riderMass,
-                bikeMass: athleteData.bikeMass,
-                otherMass: athleteData.otherMass,
-                cp: athleteData.cp,
-                wPrime: athleteData.wPrime,
-            }),
+            body: JSON.stringify(backendAthlete),
         },
     ).then(async (response) => {
         if (!response.ok) {
@@ -77,20 +86,14 @@ export function updateAthlete(
 export function createAthlete(
     athleteData: Omit<AthleteData, 'id'>,
 ): Promise<number> {
+    const backendAthlete = frontendAthleteToBackend(athleteData);
     return fetch('http://localhost:8000/server_functions/athlete/', {
         method: 'POST',
         credentials: 'include',
         headers: {
             'Content-Type': 'application/json',
         },
-        body: JSON.stringify({
-            fullName: athleteData.fullName,
-            riderMass: athleteData.riderMass,
-            bikeMass: athleteData.bikeMass,
-            otherMass: athleteData.otherMass,
-            cp: athleteData.cp,
-            wPrime: athleteData.wPrime,
-        }),
+        body: JSON.stringify(backendAthlete),
     }).then(async (response) => {
         if (response.ok) {
             return (await response.json()).athlete_id;
@@ -103,4 +106,34 @@ export function createAthlete(
             }
         }
     });
+}
+
+function frontendAthleteToBackend(
+    frontendAthlete: AthleteData,
+): BackendAthlete {
+    return {
+        id: frontendAthlete.id,
+        name: frontendAthlete.fullName,
+        rider_mass: frontendAthlete.riderMass,
+        bike_mass: frontendAthlete.bikeMass,
+        other_mass: frontendAthlete.otherMass,
+        total_mass:
+            frontendAthlete.riderMass +
+            frontendAthlete.bikeMass +
+            frontendAthlete.otherMass,
+        CP_FTP: frontendAthlete.cp,
+        W_prime: frontendAthlete.wPrime,
+    };
+}
+
+function backendAthleteToFrontend(backendAthlete: BackendAthlete): AthleteData {
+    return {
+        id: backendAthlete.id,
+        fullName: backendAthlete.name,
+        riderMass: backendAthlete.rider_mass,
+        bikeMass: backendAthlete.bike_mass,
+        otherMass: backendAthlete.other_mass,
+        cp: backendAthlete.CP_FTP,
+        wPrime: backendAthlete.W_prime,
+    };
 }
