4bd2d3e194d060586d38aad34ecb272859207eaf
Zoe Isabelle Stevens <zoe.stevens@student.adelaide.edu.au>
2022-09-12 10:03:23 +0930

    added return statement in delete athlete

--- service_layer/backend/server_functions/views.py
+++ service_layer/backend/server_functions/views.py
@@ -144,6 +144,7 @@ def athlete_view(request, athlete_id):
     elif request.method == "DELETE":
         athlete = Athlete.objects.get(id=athlete_id)
         athlete.delete()
+        return HttpResponse(status=200)
 
 def all_athletes_view(request):
     if not request.user.is_authenticated:
