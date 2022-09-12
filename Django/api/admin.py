4c66681921e404a102997c3fcd6028da6ae24e7d
Rusty <redrusty12@gmail.com>
2022-09-12 19:45:04 +0930

    Updated athlete model in new backend Updated athlete migrations

--- Django/api/admin.py
+++ Django/api/admin.py
@@ -1,8 +1,8 @@
 from django.contrib import admin
-
 from .models import Athlete
 
-# Register your models here.
+
 @admin.register(Athlete)
 class AthleteAdmin(admin.ModelAdmin):
-    list_display = ['name','bike_mass','rider_other','total_mass','CP_FTP','W_prime']
+    list_display = ['name', 'bike_mass', 'rider_mass',
+                    'other_mass', 'total_mass', 'CP_FTP', 'W_prime']
--- Django/api/models.py
+++ Django/api/models.py
@@ -1,10 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
 class Athlete(models.Model):
     name = models.CharField(max_length=30)
     bike_mass = models.FloatField()
-    rider_other = models.FloatField(null=True)
+    rider_mass = models.FloatField(null=True)
+    other_mass = models.FloatField(null=True)
     total_mass = models.FloatField()
     CP_FTP = models.FloatField()
     W_prime = models.FloatField()
