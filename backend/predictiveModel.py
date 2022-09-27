cf6da53d7d41c555fd1f4837b0f574a646432231
AlexiGrig <alexi.grigoriadis@student.adelaide.edu.au>
2022-09-27 16:41:33 +0930

    Shell code for predictive_model
    - Finished predict_entire_course
    - Shell code for predict_single_timestep
    - Made temporaryModels.py to aid testing and create function output classes

--- backend/predictiveModel.py
+++ backend/predictiveModel.py
@@ -84,45 +84,60 @@ Accel = Propulsive_force / (Mass_total+(MoI_whl_front/Wheel_radius^2)+(MoI_whl_r
 (Accel determines the next speed, as stated above)
 
 
-
+The amount of energy left in your bucket doesn't actually form part of your speed calculations - they calculate the speed separately, and then a separate bit of code makes sure the "amount left" (W') doesn't become negative at any point.
+Amount left in the bucket is in the column Calcs!AA
 """
 
 
-
-# The amount of energy left in your bucket doesn't actually form part of your speed calculations - they calculate the speed separately, and then a separate bit of code makes sure the "amount left" (W') doesn't become negative at any point.
-
-course = {
-    "static": { # inputs that are the same for the entire course
-        "bike plus rider": {"mass rider": 71, "mass bike": 7, "mass other": 1, "crr": 0.0025, "mechanical efficiency": 0.98, "mol whl front": 0.08, "mol whl rear": 0.08, "wheel radius": 0.335},
-        "cp_model": {"cp": 430, "w'": 35000, "w' recovery function": 1, "below steady state max slope": -0.01, "below steady state power usage": 0.02, "over threshold min slope": 0.075, "over threshold power usage": 1.1, "steady state power usage": 0.91},
-        "position": {"climbing CDA increment": 0.04, "climbing min slope": 0.3, "descending CDA increment": -0.005, "descending max slope": -0.01},
-        "environment": {"wind direction": 30, "wind speed m/s": 2, "density": 1.13},
-        "technical": {"timestep size": 0.5, "starting distance": 0.1, "starting speed": 0.3}
-    }, "dynamic": { # inputs that change throughout the course
-        "lat": [35.37245, 35.37262, 35.37277, 35.37292, 35.37307, 35.37323, 35.37338, 35.37353, 35.37368, 35.37384, 35.37399, 35.37414, 35.37429, 35.37445, 35.37461, 35.37477, 35.37493, 35.37508, 35.37525, 35.37541, 35.37558, 35.37574, 35.37591, 35.37608, 35.37625, 35.37641, 35.37657, 35.3767, 35.37672, 35.37662, 35.37657, 35.37659, 35.37673, 35.3769],
-        "lon": [138.92742, 138.9279, 138.92813, 138.92838, 138.92862, 138.92886, 138.9291, 138.92935, 138.92959, 138.92981, 138.93004, 138.93027, 138.93049, 138.9307, 138.93092, 138.93114, 138.93136, 138.9316, 138.93183, 138.93205, 138.93228, 138.93251, 138.93275, 138.93298, 138.93323, 138.93346, 138.93366, 138.93384, 138.93401, 138.93414, 138.93432, 138.93452, 138.93464, 138.93471],
-        "ele": [593, 593.1, 593.3, 593, 592.3, 591.7, 591.7, 591.5, 591.3, 591.3, 591.3, 591.3, 591, 591.5, 591.6, 591.8, 591.8, 591.6, 591.5, 591.5, 591.3, 591, 591.2, 591, 590.4, 589.9, 589.4, 589.2, 588.6, 588.1, 588, 588.4, 589.2, 590.1],
-        "distance": [None, 47.4, 74.2, 102.3, 129.7, 157.8, 185.2, 213.4, 240.8, 267.5, 294.2, 320.9, 346.9, 373, 399.7, 426.4, 453.2, 480.6, 508.7, 535.5, 563.6, 591, 619.8, 648, 677.5, 704.9, 730.3, 752.1, 767.7, 783.9, 801.1, 819.4, 838.4, 858.3],
-        # in the sheet, bearing and slope are calculated values
-        # but I think it'd be more convenient to put them in gpx_to_json()
-        # so you don't have to recompute them each time
-        "bearing from prev": [None, 73.89, 62.00, 63.93, 63.00, 61.47, 63.00, 63.93, 63.00, 59.33, 62.00, 62.00, 60.93, 58.15, 59.33, 59.33, 59.33, 63.00, 58.92, 59.33, 58.92, 60.44, 59.99, 58.92, 60.99, 60.44, 56.88, 59.51, 84.52, 122.1, 102.7, 85.34, 46.43, 26.79],
-        "slope from prev": [None, 0.002, 0.007, -0.011, -0.026, -0.021, 0.000, -0.007, -0.007, 0.000, 0.000, 0.000, -0.012, 0.019, 0.004, 0.007, 0.000, -0.007, -0.004, 0.000, -0.007, -0.011, 0.007, -0.007, -0.020, -0.018, -0.020, -0.009, -0.038, -0.031, -0.006, 0.022, 0.042, 0.045]
-    }
-} # The data is being stored as {data type: [all timesteps]}, just because it makes it easier to write the above code
+from temporaryModels import *
+import math
 
 
+def predict_single_timestep(course: CourseModel,
+                   time: float, # total time so far, starts at 0
+                   w_prime_balance: float,
+                   distance: float,
+                   speed: float,
+                   acceleration: float) \
+                   -> SingleTimestepOutput:
 
-print(course)
 
-# def singleTimestep(time=0, dist=0, speed=0.3, slope=0, roughness = 0,
-#                    windSpeed=2, windAngle=math.pi, #radians, where 0 = headwind
-#                    timestepSize=1):
-#     headwindSpeed = math.cos(windAngle) * windSpeed
-#     relativeHeadwind = speed + headwindSpeed
-#     windDensity =
-#     #powerAero = 0.5 * "relative wind speed at 1m"^3 * CDA
-#     ## within that:
-#     ## "relative wind speed at 1m" = a guess based on the roughness???
-#     #So the wind speed
+    return SingleTimestepOutput(distance=distance+1,
+                                speed=1, # current speed is based on the previous speed and acceleration
+                                acceleration=1,
+                                w_prime_balance=1) #temp values
+
+def predict_entire_course(course) -> PredictEntireCourseOutput:
+
+    max_distance = course.dynamic.distance[-1] # last distance in list
+    current_distance = course.static.technical_m.starting_distance
+    current_speed = course.static.technical_m.starting_speed
+    current_acceleration = 0 # in meters per second per timestep
+    current_time = 0
+    current_w_prime_balance = course.static.cp_m.w_prime
+    min_w_prime_balance = float('inf')
+
+    while current_distance < max_distance:
+
+        single_out = predict_single_timestep(course=course,
+                                             time=current_time,
+                                             w_prime_balance=current_w_prime_balance,
+                                             distance=current_distance,
+                                             speed=current_speed,
+                                             acceleration=current_acceleration)
+
+        current_distance = single_out.distance
+        current_speed = single_out.speed
+        current_acceleration = single_out.acceleration
+        current_time += course.static.technical_m.timestep_size
+        min_w_prime_balance = min(current_w_prime_balance, single_out.w_prime_balance)
+        current_w_prime_balance = single_out.w_prime_balance
+
+    return PredictEntireCourseOutput(duration=current_time, min_w_prime_balance=min_w_prime_balance)
+
+predict_entire_course(CourseModel())
+
+
+
+
 
---
+++ backend/temporaryModels.py
@@ -0,0 +1,95 @@
+
+# WHY THIS FILE EXISTS:
+# In the final version of this program,
+# the predictiveModel will take the classes listed in Django/api/models.py
+# but I can't import that into my code without everything breaking for some reason.
+# So in the meantime, this file duplicates the functions that are in that file,
+# with the only difference being that the fields like models.FloatField()
+# are replaced with plausible values for that field.
+# Also I removed "_model" as a suffix for some fields, for ease of reading/writing
+# (this can easily be fixed by automatic refactoring).
+
+
+
+
+
+class BikePlusRiderModel():
+    mass_rider = 71
+    mass_bike = 7
+    mass_other = 1
+    crr = 0.0025
+    mechanical_efficiency = 0.98
+    mol_whl_front = 0.08
+    mol_whl_rear = 0.08
+    wheel_radius = 0.335
+
+class CPModel():
+    cp = 430
+    w_prime = 35000 # how much you start with in the bucket ('current bucket amount' is called balance and is not in this model).
+    w_prime_recovery_function = 1
+    below_steady_state_max_slope = -0.01
+    below_steady_state_power_usage = 0.02
+    over_threshold_min_slope = 0.075
+    over_threshold_power_usage = 1.1
+    steady_state_power_usage = 0.91
+
+class PositionModel():
+    climbing_cda_increment = 0.04
+    climbing_min_slope = 0.3
+    descending_cda_increment = -0.005
+    descending_max_slope = -0.01
+
+class EnvironmentModel():
+    wind_direction = 30
+    wind_speed_mps = 2
+    wind_density = 1.13
+
+class TechnicalModel():
+    timestep_size = 0.5
+    starting_distance = 0.1
+    starting_speed = 0.3
+
+class StaticModel():
+    bike_plus_rider_m = BikePlusRiderModel()
+    cp_m = CPModel()
+    position_m = PositionModel()
+    environment_m = EnvironmentModel()
+    technical_m = TechnicalModel()
+
+class DynamicModel():
+    lat = [35.37245, 35.37262, 35.37277, 35.37292, 35.37307, 35.37323, 35.37338, 35.37353, 35.37368, 35.37384, 35.37399, 35.37414, 35.37429, 35.37445, 35.37461, 35.37477, 35.37493, 35.37508, 35.37525, 35.37541, 35.37558, 35.37574, 35.37591, 35.37608, 35.37625, 35.37641, 35.37657, 35.3767, 35.37672, 35.37662, 35.37657, 35.37659, 35.37673, 35.3769]
+    lon = [138.92742, 138.9279, 138.92813, 138.92838, 138.92862, 138.92886, 138.9291, 138.92935, 138.92959, 138.92981, 138.93004, 138.93027, 138.93049, 138.9307, 138.93092, 138.93114, 138.93136, 138.9316, 138.93183, 138.93205, 138.93228, 138.93251, 138.93275, 138.93298, 138.93323, 138.93346, 138.93366, 138.93384, 138.93401, 138.93414, 138.93432, 138.93452, 138.93464, 138.93471]
+    ele = [593, 593.1, 593.3, 593, 592.3, 591.7, 591.7, 591.5, 591.3, 591.3, 591.3, 591.3, 591, 591.5, 591.6, 591.8, 591.8, 591.6, 591.5, 591.5, 591.3, 591, 591.2, 591, 590.4, 589.9, 589.4, 589.2, 588.6, 588.1, 588, 588.4, 589.2, 590.1]
+    distance = [None, 47.4, 74.2, 102.3, 129.7, 157.8, 185.2, 213.4, 240.8, 267.5, 294.2, 320.9, 346.9, 373, 399.7, 426.4, 453.2, 480.6, 508.7, 535.5, 563.6, 591, 619.8, 648, 677.5, 704.9, 730.3, 752.1, 767.7, 783.9, 801.1, 819.4, 838.4, 858.3]
+    # in the sheet, bearing and slope are calculated values
+    # but I think it'd be more convenient to put them in gpx_to_json()
+    # so you don't have to recompute them each time
+    bearing_from_prev = [None, 73.89, 62.00, 63.93, 63.00, 61.47, 63.00, 63.93, 63.00, 59.33, 62.00, 62.00, 60.93, 58.15, 59.33, 59.33, 59.33, 63.00, 58.92, 59.33, 58.92, 60.44, 59.99, 58.92, 60.99, 60.44, 56.88, 59.51, 84.52, 122.1, 102.7, 85.34, 46.43, 26.79]
+    slope_from_prev = [None, 0.002, 0.007, -0.011, -0.026, -0.021, 0.000, -0.007, -0.007, 0.000, 0.000, 0.000, -0.012, 0.019, 0.004, 0.007, 0.000, -0.007, -0.004, 0.000, -0.007, -0.011, 0.007, -0.007, -0.020, -0.018, -0.020, -0.009, -0.038, -0.031, -0.006, 0.022, 0.042, 0.045]
+
+class CourseModel():
+    static = StaticModel()
+    dynamic = DynamicModel()
+
+class SingleTimestepOutput():
+    def __init__(self,
+                 next_distance,
+                 next_speed,
+                 next_w_prime_balance # the amount 'left in the bucket'
+                 ):
+        self.distance = distance
+        self.speed = speed
+        self.acceleration = acceleration
+        self.w_prime_balance = w_prime_balance
+
+class PredictEntireCourseOutput():
+    def __init__(self,
+                 duration,
+                 min_w_prime_balance
+                 ):
+        self.duration = duration # how long the entire course took
+        self.min_w_prime_balance = min_w_prime_balance # the lowest the bucket went (the coaches will adjust all the values until this is close to zero, but not negative, meaning that the athlete never ran out).
+
+
+
+
