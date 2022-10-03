ee32b89a2f16f663733d8ec6e9851ce9ac6f22cf
Zoe Stevens <zoestevens@Zoes-Air-2.modem>
2022-10-03 20:41:28 +1030

    finished PUT request for StaticModel
    Works for StaticModel shown in models.py, however if the Static model has been changed since then (i.e. merged together) this will need to be rewritten
    
    Also added Delete model

--- Django/backend/urls.py
+++ Django/backend/urls.py
@@ -34,5 +34,5 @@ urlpatterns = [
     #path('api/csrf', views.csrf,name='csrf'),
     #path('api/login', views.empty_login,name='login'),
     path('api/staticmodels', views.all_static_model_view, name='static_models'),
-    path('api/staticmodels/<int:static_model_id>', views.static_model_view, name='static_models')
+    path('api/staticmodels/<int:gpx_model_id>', views.static_model_view, name='static_models')
 ]
