7e604f8bbb9f2f68e90ff58df689476b06a80dd5
rezaim0 <39697511+rezaim0@users.noreply.github.com>
2022-10-03 15:35:14 +1030

    predict_powerin.py
    writing the function

--- backend/predictiveModels/predictPowerIn.py
+++ backend/predictiveModels/predictPowerIn.py
@@ -1,6 +1,24 @@
+#from symbol import power
 from temporaryModels import *
 
 def predict_power_in(course: CourseModel, distance: float, speed: float, acceleration: float) -> float:
-    # TODO: this function
-    # some arguments may not be necessary - once the code has been finished, we can get rid of whatever arguments we don't need
-    return 100
+    
+    index = 0
+    for checkedIndex in reversed(range(len(course.dynamic.distance))):
+        if course.dynamic.distance[checkedIndex] <= distance:
+            index = checkedIndex
+        break
+    course.dynamic.slope_from_prev[0] = 0
+    slope = course.dynamic.slope_from_prev[index]
+
+    if(slope < course.static.below_steady_state_max_slope):
+        v_lookup = course.static.cp*0.02
+    
+    elif(slope >  course.static.over_threshold_min_slope):
+         v_lookup = course.static.cp*0.91
+    else:
+         v_lookup = course.static.cp*1.1
+
+    power_in = course.static.mechanical_efficiency * v_lookup + course.static.delta_watts
+    
+    return power_in
