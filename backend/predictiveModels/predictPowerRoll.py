baebc33e25c7245625324c41bb840f397f830881
AlexiGrig <alexi.grigoriadis@student.adelaide.edu.au>
2022-10-03 17:47:30 +1030

    Made roughness accurate, fixed a bug in predictPowerRoll

--- backend/predictiveModels/predictPowerRoll.py
+++ backend/predictiveModels/predictPowerRoll.py
@@ -5,6 +5,6 @@ def predict_power_roll(course: CourseModel, speed: float) -> float:
     cs = course.static # for brevity
 
     mass_total = cs.mass_rider + cs.mass_bike + cs.mass_other + cs.delta_kg
-    power_roll = mass_total + cs.crr + speed
+    power_roll = mass_total * cs.crr * 9.81 * speed
 
     return power_roll
