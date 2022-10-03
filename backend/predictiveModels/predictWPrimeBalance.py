b3febc296e79b002174373506d78a63ef717d778
AlexiGrig <alexi.grigoriadis@student.adelaide.edu.au>
2022-10-03 17:20:54 +1030

    Fixed a bug and fixed an error in the pseudocode

--- backend/predictiveModels/predictWPrimeBalance.py
+++ backend/predictiveModels/predictWPrimeBalance.py
@@ -6,7 +6,7 @@ def predict_w_prime_balance(course: CourseModel, prev_w_prime_balance, power_in)
 
     cs = course.static # for brevity
 
-    dcp = min(0, -((power_in*(1/cs.mechanical_efficiency))-cs.cp))
+    dcp = max(0, -((power_in*(1/cs.mechanical_efficiency))-cs.cp))
 
     if (power_in*(1/cs.mechanical_efficiency))-cs.cp > 0:
         ot_energy = power_in * (1/cs.mechanical_efficiency) * cs.timestep_size
@@ -14,17 +14,20 @@ def predict_w_prime_balance(course: CourseModel, prev_w_prime_balance, power_in)
         ot_energy = 0
 
     if cs.w_prime_recovery_function == 1:
-        if dcp != 0:
+        try:
             t_prime = 2287.2 * dcp ** -0.688
-        else:
+        except:
             t_prime = 0
     elif cs.w_prime_recovery_function == 2:
-        if dcp != 0:
+        try:
             t_prime = cs.w_prime / dcp
-        else:
+        except:
             t_prime = 0
     elif cs.w_prime_recovery_function == 3:
-        t_prime = 546 * (math.e ** (-0.01 * dcp)) + 316
+        try:
+            t_prime = 546 * (math.e ** (-0.01 * dcp)) + 316
+        except:
+            t_prime = 0
     else:
         t_prime = 0
         # cs.w_prime_recovery_function should ALWAYS be 1, 2 or 3, and in an ideal world should throw an error if it's not - but the sheet supresses that error by setting t_prime to 0 instead
